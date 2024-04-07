b946a6ddf5f0d80ebfff8b26427ec0c2adf5d5e7
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-04-07 20:08:09 +0530

    Create a navbar section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>My portfolio</h1>
+      <Navbar />
+      <Hero />
     </>
   );
 }
