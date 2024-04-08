3caf4a33451f1bc48d9e5feb42f65962d0a3f6ef
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-04-09 00:05:28 +0530

    Add some functionality to navbar section
    when the click each nav tag set the color effect and animations

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/react.svg';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState('home');
+
+  const liHandler = (menuItem) => {
+    setMenu(menuItem);
+  };
   return (
     <div className="navbar">
       <img src={logo} alt="myLogo" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li
+          className={menu === 'home' ? 'underline' : 'noUnderline'}
+          onClick={() => liHandler('home')}
+        >
+          Home
+        </li>
+        <li
+          className={menu === 'about' ? 'underline' : 'noUnderline'}
+          onClick={() => liHandler('about')}
+        >
+          About Me
+        </li>
+        <li
+          className={menu === 'services' ? 'underline' : 'noUnderline'}
+          onClick={() => liHandler('services')}
+        >
+          Services
+        </li>
+        <li
+          className={menu === 'work' ? 'underline' : 'noUnderline'}
+          onClick={() => liHandler('work')}
+        >
+          Portfolio
+        </li>
+        <li
+          className={menu === 'contact' ? 'underline' : 'noUnderline'}
+          onClick={() => liHandler('contact')}
+        >
+          Contact
+        </li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
     </div>
