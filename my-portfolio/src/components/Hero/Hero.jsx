75335f4682a42c42604ebd1ff68a226d363963dc
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-04-08 11:02:52 +0530

    Complete Hero section
    We have to add images. Its do that final

--- my-portfolio/src/components/Hero/Hero.jsx
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './Hero.css';
+import profileImage from '../../assets/react.svg';
 
 const Hero = () => {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <img src={profileImage} alt="My image" />
+      <h1>
+        <span>I'm Indrajith Bodhinayaka</span>, Software Engineer
+      </h1>
+      <p>I have a experience 3 years to Fullstack development</p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
