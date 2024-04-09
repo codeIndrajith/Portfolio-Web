d4dd700d034a17f23a4d933862e01b4be32bda2e
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-04-09 13:49:35 +0530

    Send contact data to my email

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,7 +6,6 @@ import Hero from './components/Hero/Hero';
 import Mywork from './components/My Work/Mywork';
 import Navbar from './components/Navbar/Navbar';
 import Services from './components/Services/Services';
-
 function App() {
   return (
     <>
--- my-portfolio/src/components/Contact/Contact.jsx
+++ my-portfolio/src/components/Contact/Contact.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 import theme_pattern from '../../assets/react.svg';
 import location_icon from '../../assets/location_icon.svg';
 import call_icon from '../../assets/call_icon.svg';
 import mail_icon from '../../assets/mail_icon.svg';
 
+const PUBLIC_ACCESS_KEY = import.meta.env.VITE_PUBLIC_ACCESS_KEY;
+
 const Contact = () => {
+  const [name, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append('access_key', PUBLIC_ACCESS_KEY);
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log('Success', res);
+      setUserName('');
+      setEmail('');
+      setMessage('');
+    }
+  };
+
   return (
     <div id="contact" className="contact">
       <div className="contact-title">
@@ -32,17 +64,31 @@ const Contact = () => {
           </div>
         </div>
 
-        <form className="contact-right">
+        <form onSubmit={onSubmit} className="contact-right">
           <label htmlFor="">Your Name</label>
-          <input type="text" placeholder="Enter Your Name" name="name" />
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            name="name"
+            value={name}
+            onChange={(e) => setUserName(e.target.value)}
+          />
           <label htmlFor="">Your Email</label>
-          <input type="email" placeholder="Enter Your Email" />
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label htmlFor="">Write Message</label>
           <textarea
             name="message"
+            value={message}
             id=""
             rows="8"
             placeholder="Enter Your Message"
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
           <button type="submit" className="contact-submit">
             Submit now
